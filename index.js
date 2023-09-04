6a560e3ac71c958ea767f6c3d7d7cfca659b1f09
Luca Pradovera <luca.pradovera@gmail.com>
2023-09-04 12:30:53 +0200

    Working outbound minimal and full clients

--- index.js
+++ index.js
@@ -23,8 +23,12 @@ async function apiRequest(endpoint, payload = {}, method = 'POST') {
 
 app.get('/', async (req, res) => {
   var token = await apiRequest('/api/fabric/subscribers/tokens', { reference: 'myclient' })
-  console.log(token)
   res.render('index', { token });
+});
+
+app.get('/minimal', async (req, res) => {
+  var token = await apiRequest('/api/fabric/subscribers/tokens', { reference: 'myclient' })
+  res.render('minimal', { token });
 });
 
 app.listen(process.env.PORT || 3000, () => {
